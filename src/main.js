358dd39c0fb959ccd41a6e96f7da07fbf694906d
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-03-03 11:23:53 -0500

    fix reloading issue

--- src/main.js
+++ src/main.js
@@ -10,10 +10,11 @@ function getPossible(n) {
 }
 
 // Construct a box within a Sudoku board based on board state.
-function createBox(n, selected, defaultVal, val) {
+function createBox(n, selected, defaultVal, val, i) {
+    let nsqr = n*n;
     let boxClass = selected ? "selectedbox" : (defaultVal ? "defaultbox" : "unselectedbox");
     let innerVal = val === null ? "" : val;
-    let size = Math.floor(100 / (n*n));
+    let size = Math.floor(100 / (nsqr));
     let padVal = 0;
     switch (n) {
         case 2: padVal = 30; break;
@@ -21,7 +22,19 @@ function createBox(n, selected, defaultVal, val) {
         case 4: padVal = 15; break;
         case 5: padVal = 6; break;
     }
-    return `<div class="${boxClass}" style="padding: ${padVal}%;">${innerVal}</div>\n`;
+    let style = `padding: ${padVal}%;`;
+    
+    let isTop = (Math.floor(i / nsqr) % n === 0);
+    let isLeft = ((i % nsqr) % n === 0);
+    let isRight = (((i+1) % nsqr) % n === 0);
+    let isBottom = (Math.floor((i+nsqr) / nsqr) % n === 0);
+    
+    if (isTop) { style += " border-top: 2px solid black;"; }
+    if (isLeft) { style += " border-left: 2px solid black;"; }
+    if (isRight) { style += " border-right: 2px solid black;"; }
+    if (isBottom) { style += " border-bottom: 2px solid black;"; }
+    
+    return `<div class="${boxClass}" style="${style}">${innerVal}</div>\n`;
 }
     
 // Create a grid given a Sudoku base, HTML id, current selection location, and default and entered values.
@@ -32,7 +45,7 @@ function createGrid(n, id, selectRow, selectCol, defaultVals, enteredVals) {
         let selected = selectRow * nsqr + selectCol === i;
         let defaultVal = defaultVals[i] !== null;
         let val = defaultVal ? defaultVals[i] : enteredVals[i];
-        gridContents += createBox(n, selected, defaultVal, val);
+        gridContents += createBox(n, selected, defaultVal, val, i);
     }
     document.getElementById(id).style.setProperty("grid-template-columns", `repeat(${n*n}, 1fr)`);
     document.getElementById(id).style.setProperty("grid-template-rows", `repeat(${n*n}, 1fr)`);
@@ -58,6 +71,7 @@ function makeBoard(n) {
     let defaultVals = board.defaultVals;
     let enteredVals = board.enteredVals;
     let winVal = document.getElementById("winstate");
+    winVal.innerHTML = "";
 
     createGrid(n, id, row, col, defaultVals, enteredVals);
     window.addEventListener("keydown", (event) => {
@@ -67,17 +81,17 @@ function makeBoard(n) {
         else if (event.key === "ArrowRight") col = col === (nsqr) - 1 ? col : col + 1;
         else if (event.key === "ArrowLeft") col = col === 0 ? col : col - 1;
         
-        // Control guess entry logic
-        else if (possible.includes(event.key.toUpperCase())) enteredVals[row * nsqr + col] = event.key.toUpperCase();
-        else if (event.key === "Backspace") enteredVals[row * nsqr + col] = "";
-        
         // Check for win state
         if (!won) {
+            // Control guess entry logic
+            if (possible.includes(event.key.toUpperCase())) enteredVals[row * nsqr + col] = event.key.toUpperCase();
+            else if (event.key === "Backspace") enteredVals[row * nsqr + col] = "";
+            
             won = isCorrect(enteredVals, board);
             if (won) { winVal.innerHTML = "Congrats! :)"; }
-            else { createGrid(n, id, row, col, defaultVals, enteredVals); }
         }
-        else {}
+        
+        createGrid(n, id, row, col, defaultVals, enteredVals);
     });
 }
 
@@ -86,10 +100,10 @@ function main() {
     let generatorButton = document.getElementById("generator");
     
     let slider = document.getElementById("sideslider");
-    let sliderVal = document.getElementById("sliderval");
-    sliderVal.innerHTML = "Base: " + slider.value;
+    let sliderVal = document.getElementById("basenumber");
+    sliderVal.innerHTML = "n";
     slider.oninput = function() {
-        sliderVal.innerHTML = "Base: " + this.value;
+        sliderVal.innerHTML = this.value;
         n = parseInt(this.value);
     }
     
